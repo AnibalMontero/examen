ec521d688827d76932be0f7337a7761bc385313e
Anibal Montero <anibal.montero.p@gmail.com>
2022-05-25 09:18:21 +0200

    Inicio de proyecto

---
+++ ejercicio.js
@@ -0,0 +1,1 @@
+//Usando la API de emojis de GitHub muestra todos los emojis (su imagen en un nodo HTML cada una) que contengan una "y" en su nombre (las claves del JSON de respuesta contienen los nombres de los emojis)
